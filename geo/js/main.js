7e81df99cd25c3bd993299b5861f5b5a98d328dd
willianifms <willian.santos5@estudante.ifms.edu.br>
2023-11-27 16:32:24 -0400

    barra de pesquisa maps

--- geo/js/main.js
+++ geo/js/main.js
@@ -1,46 +1,29 @@
+
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', async () => {
     try {
       let reg;
-      reg = await navigator.serviceWorker.register('./sw.js', {type: "module"});
+      reg = await navigator.serviceWorker.register('./sw.js', { type: "module" });
       console.log('Service worker registrada!', reg);
     } catch (err) {
       console.log(' Service worker registro falhou:', err);
     }
   });
-};
+}
 
-let posicaoInicial; //variavel para capturar a posicao
-const capturarLocalizacao = document.getElementById('localizacao');
-const latitude = document.getElementById('latitude');
-const longitude = document.getElementById('longitude');
+const capturarLocalizacao = document.getElementById('pesquisarLocalizacao');
+const inputLocalizacao = document.getElementById('inputLocalizacao');
 const map = document.getElementById('mapa');
-const sucesso = (posicao) => {//callback de sucesso para captura da posicao
-  posicaoInicial = posicao;
-  latitude.innerHTML = posicaoInicial.coords.latitude;
-  longitude.innerHTML = posicaoInicial.coords.longitude;
-  map.src = "https://maps.google.com/maps?q=" + posicaoInicial.coords.latitude + ", " + posicaoInicial.coords.longitude + "&z=16&output=embed"
-}
 
-
-const erro = (error) => {//callback ite error (falha para captura de Localizacao)
-  let errorMessage;
-  switch(error.code){
-    case 0:
-      errorMessage = "Erro desconhecido"
-    break;
-    case 1:
-      errorMessage = "Permissão negada!"
-    break;
-    case 2:
-      errorMessage = "Captura de posição indisponivel!"
-    break;
-    case 3:
-      errorMessage = "Tempo de solicitação excedido!"
-    break ;
-  }
-  console.log('Ocorreu um erro: ' + errorMessage);
-}
 capturarLocalizacao.addEventListener('click', () => {
-  navigator.geolocation.getCurrentPosition(sucesso, erro);
+  const localizacaoDigitada = inputLocalizacao.value;
+  if (localizacaoDigitada) {
+    // Construir a URL do Google Maps com base na localização digitada
+    const mapUrl = `https://maps.google.com/maps?q=${encodeURIComponent(localizacaoDigitada)}&z=16&output=embed`;
+    
+    // Exibir o mapa no iframe
+    map.src = mapUrl;
+  } else {
+    alert("Por favor, digite uma localização válida.");
+  }
 });
